b99de9c4bfff8941c079887b59ccd8c58fcc1e57
aivat <aivat@mail.ru>
2018-12-11 17:54:48 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header class="header">
+      <h3>Todos</h3>
+      <input autofocus autocomplete="off" placeholder="Добавьте новый элемент" @keyup.enter="addTodo">    
+    </header>
+    <main>
+      <ul>
+        <TodoItem v-for="(todo, index) in filteredTodos" :key="index" :todo="todo"/> 
+      </ul>
+      <button @click="done = !done">{{ done ? 'Посмотреть новые' : 'Посмотреть исполненные' }}</button>  
+    </main>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import TodoItem from '@/components/TodoItem.vue'
+import { ITodo, ITodoGetter, IAddTodoAction } from './types'
+import Todo  from './store/modules/todo'
+const todoModule = getModule(Todo)
+const commentCount = todoModule.filteredTodos(false)
+@Component({
+  components: { TodoItem } 
+})
+export default class App extends Vue {
+  
+  done: boolean = false
+  get hotels() {
+    return this.$store.state.todo.to
+  }
+  // get filteredTodos() {
+  //   return this.$store.getters.filteredTodos(this.done)
+  // }
+   get filteredTodos() {
+    return commentCount
+  } 
+  addTodo (e:any) {
+    const text = e.target.value
+    if (text.trim()) {
+      this.$store.dispatch('addTodo', text)
+    }
+    e.target.value = ''
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
